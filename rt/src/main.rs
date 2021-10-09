a441743f4cde5ec69f4791b111c10e90609f1769
Benjamin Wilhelm <luigi_93@gmx.de>
2021-10-09 09:32:18 +0200

    test

--- rt/src/main.rs
+++ rt/src/main.rs
@@ -5,27 +5,25 @@ use core::panic::PanicInfo;
 use core::ptr;
 fn foo() {
     let rcc_ahbenr = 0x40021000 | 0x14;
-    unsafe {
-        ptr::write_volatile(rcc_ahbenr as *mut u32, 1 << 17)
-    }
+    unsafe { ptr::write_volatile(rcc_ahbenr as *mut u32, 1 << 17) }
 
     let gpioa_base = 0x48000000;
     let gpioa_moder = gpioa_base | 0x00;
     unsafe {
         let mut existing_val = ptr::read_volatile(gpioa_moder as *const u32);
+
+        // // clear out first bit (LSB)
+        // existing_val &= !(1 << 0);
+
+        // // clear out second bit (LSB)
+        // existing_val &= !(1 << 1);
+
+        // // set bit 0 & 1 to 0b01 (see p. 237, "01: General purpose output mode") first bit (LSB)
+        // existing_val |= (0b01) | (0b00);
         existing_val = existing_val & !(0b11) & !(0b11); 
 
-        ptr::write_volatile(
-            gpioa_moder as *mut u32, 
-            existing_val | (0b01) | (0b00)
-        );
+        ptr::write_volatile(gpioa_moder as *mut u32,existing_val | (0b01) | (0b00));
     }
-    // unsafe {
-    //     // see p 237
-    //     // 01: General purpose output mode
-    //     ptr::write_volatile(gpioa_base as *mut u32, 1)
-    // }
-
     let gpioa_otyper = gpioa_base | 0x04;
     unsafe {
         // see p 237
@@ -46,8 +44,6 @@ fn foo() {
         ptr::write_volatile(gpioa_odr as *mut u32, 0)
     }
 }
-
-
 
 #[no_mangle]
 pub unsafe extern "C" fn Reset() -> ! {
