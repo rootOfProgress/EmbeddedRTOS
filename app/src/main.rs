68f5be84fe0d7bc218804fa10adb822a2f8bf9e5
turbokalle <lukas.kaluscha@gmail.com>
2021-10-12 01:05:50 +0200

    minimal code for led

--- app/src/main.rs
+++ app/src/main.rs
@@ -11,7 +11,7 @@ static mut DATA: u16 = 1;
 
 const RCC_AHBENR: u32 = 0x4002_1000 | 0x14;
 
-const GPIOE_BASE: u32 = 0x4800_0000;
+const GPIOE_BASE: u32 = 0x4800_1000;
 
 #[repr(C)]
 struct RCC {
@@ -20,17 +20,17 @@ struct RCC {
 
 #[repr(C)]
 struct GPIO {
-    moder: u32,
-    otyper: u32,
-    ospeedr: u32,
-    pupdr: u32,
-    idr: u32,
-    odr: u32,
-    bsrr: u32,
-    lckr: u32,
-    afrl: u32,
-    afrh: u32,
-    brr: u32,
+    pub moder: u32,
+    pub otyper: u32,
+    pub ospeedr: u32,
+    pub pupdr: u32,
+    pub idr: u32,
+    pub odr: u32,
+    pub bsrr: u32,
+    pub lckr: u32,
+    pub afrl: u32,
+    pub afrh: u32,
+    pub brr: u32,
 }
 
 #[no_mangle]
@@ -39,17 +39,16 @@ fn main() -> ! {
     let rcc = unsafe { &mut *(RCC_AHBENR as *mut RCC) };
 
     // IOPAEN p.166 "io port a enable"
-    unsafe {ptr::write_volatile(&mut (*rcc).ahbenr as *mut u32, ptr::read_volatile(&mut rcc.ahbenr) | 1 << 17)};
+    unsafe {ptr::write_volatile(&mut rcc.ahbenr as *mut u32, ptr::read_volatile(&mut rcc.ahbenr) | (1 << 17 | 1 << 21))};
     
     let gpioe = unsafe { &mut *(GPIOE_BASE as *mut GPIO) };
     
+    let pin = 9;
     unsafe {
-        ptr::write_volatile(&mut gpioe.moder as *mut u32, ptr::read_volatile(&mut gpioe.moder) | (0b01 as u32) << (9 * 2));
-        ptr::write_volatile(&mut gpioe.otyper as *mut u32, ptr::read_volatile(&mut gpioe.otyper) & !(1 as u32) << 9);
-        ptr::write_volatile(&mut gpioe.odr as *mut u32, ptr::read_volatile(&mut gpioe.odr) | 1 << 9);
+        ptr::write_volatile(&mut gpioe.moder as *mut u32, ptr::read_volatile(&mut gpioe.moder) | (0b01 as u32) << (pin * 2));
+        ptr::write_volatile(&mut gpioe.otyper as *mut u32, ptr::read_volatile(&mut gpioe.otyper) & !(1 as u32) << pin);
+        ptr::write_volatile(&mut gpioe.odr as *mut u32, ptr::read_volatile(&mut gpioe.odr) | (0b1 as u32) << pin);
     }
-
-    let _mod = unsafe {ptr::read_volatile(&mut (*gpioe).moder)};
 
     let _x = RODATA;
     let _y = unsafe { &BSS };
