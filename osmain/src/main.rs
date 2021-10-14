bed4ff37141c9107e97fe44a98a00dff726287a7
Benjamin Wilhelm <luigi_93@gmx.de>
2021-10-14 17:10:29 +0200

    feat: first context switch

--- osmain/src/main.rs
+++ osmain/src/main.rs
@@ -14,24 +14,35 @@ fn context1() {
 
 #[no_mangle]
 extern "C" {
+    // fn bar(stack_ptr: *mut u32);
     fn bar(stack_ptr: *mut u32);
 }
 
 #[no_mangle]
 pub fn main() -> ! {
-    let _x = 42;
-    let mut usr_stack:  [*mut u32; 256];
+    // context1();
     unsafe {
-        usr_stack = core::mem::uninitialized();
-        usr_stack[248] = context1 as *mut u32;
+        let mut quax:  [u32; 256] = core::mem::uninitialized();
+        let ptr = &quax as *const u32;
+        // for i in 0..240 {
+        //     quax[i] = 123;
+        // }
+        // for i in 240..256 {
+        //     quax[i] = i as u32;
+        // }
+        let usertask_stack_start: *const u32 = ptr.offset(256 - 16);
+
+
+        // let thread_psp = usertask_stack_start.offset(8)  as *mut u32;
+        // quax[i] = i as u32;
+        // for i in 7..9 {
+            let fn_start = usertask_stack_start.offset(9)  as *mut u32;
+            let sdf = context1 as *const ();
+            ptr::write_volatile(fn_start, sdf as u32);
+        // }
+
+        bar(usertask_stack_start as *mut u32);
     }
-    rt::sched::scheduler::dispatch(usr_stack[240]);
-    unsafe {
-        asm! {"bkpt"}
-    }
-    // unsafe {
-    //     bar(usr_stack[240]);
-    // }
     loop {}
 }
 
--- rt/asm.s
+++ rt/asm.s
@@ -11,12 +11,9 @@ bar:
 */
 bar:
 	mrs ip, psr
-  push {R4-R11, ip,lr}
-
+  	push {R4-R11, ip,lr}
 	msr psp, r0
 	mov r0, #3
 	msr control, r0
-
 	pop {R4-R11, ip,lr}
-
 	bx lr
