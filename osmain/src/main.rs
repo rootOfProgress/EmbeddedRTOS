27229bd6a2766e3a2a376940c7aeeb17b11b1965
Benjamin Wilhelm <luigi_93@gmx.de>
2021-10-14 20:23:05 +0200

    chore: remove silly namings

--- osmain/src/main.rs
+++ osmain/src/main.rs
@@ -22,24 +22,13 @@ extern "C" {
 pub fn main() -> ! {
     // context1();
     unsafe {
-        let mut quax:  [u32; 256] = core::mem::uninitialized();
-        let ptr = &quax as *const u32;
-        // for i in 0..240 {
-        //     quax[i] = 123;
-        // }
-        // for i in 240..256 {
-        //     quax[i] = i as u32;
-        // }
-        let usertask_stack_start: *const u32 = ptr.offset(256 - 16);
+        let mut process_stack:  [u32; 256] = core::mem::uninitialized();
+        let stack_ptr = &process_stack as *const u32;
+        let usertask_stack_start: *const u32 = stack_ptr.offset(256 - 16);
 
+        let fn_start = usertask_stack_start.offset(9)  as *mut u32;
+        ptr::write_volatile(fn_start, context1 as *const () as u32);
 
-        // let thread_psp = usertask_stack_start.offset(8)  as *mut u32;
-        // quax[i] = i as u32;
-        // for i in 7..9 {
-            let fn_start = usertask_stack_start.offset(9)  as *mut u32;
-            let sdf = context1 as *const ();
-            ptr::write_volatile(fn_start, sdf as u32);
-        // }
 
         bar(usertask_stack_start as *mut u32);
     }
