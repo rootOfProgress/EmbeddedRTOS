85cb875c10d63e26996019ae3751a8f94d14f538
Benjamin Wilhelm <be631wil@htwg-konstanz.de>
2021-12-20 08:33:45 +0100

    add gitignore

--- userspace/target/debug/build/rt-199353353c07b2ad/build_script_build-199353353c07b2ad.d
+++
@@ -1,6 +0,0 @@
-/home/benni/development/os_dev/EmbeddedRTOS/userspace/target/debug/build/rt-199353353c07b2ad/build_script_build-199353353c07b2ad: /home/benni/development/os_dev/EmbeddedRTOS/rt/build.rs /home/benni/development/os_dev/EmbeddedRTOS/rt/link.x
-
-/home/benni/development/os_dev/EmbeddedRTOS/userspace/target/debug/build/rt-199353353c07b2ad/build_script_build-199353353c07b2ad.d: /home/benni/development/os_dev/EmbeddedRTOS/rt/build.rs /home/benni/development/os_dev/EmbeddedRTOS/rt/link.x
-
-/home/benni/development/os_dev/EmbeddedRTOS/rt/build.rs:
-/home/benni/development/os_dev/EmbeddedRTOS/rt/link.x:
